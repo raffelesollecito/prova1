0c61ff58f0e7fd6d185e2c257bf56669f737dba8
Giovanni Figliuolo <figliuolo.giovanni@yahoo.it>
2018-04-26 13:43:36 +0200

    Che bello

---
+++ src/DiscreteItem.java
@@ -0,0 +1,23 @@
+/**
+ * Estende la classe Item e rappresenta una coppia Attributo,valore discreto.
+ * Esempio Outlook="Sunny"
+ * @author sante
+ *
+ */
+class DiscreteItem extends Item {
+	DiscreteItem(DiscreteAttribute attribute,String value){
+		super(attribute,value);
+	}
+	
+	/**
+	 * Valuta la distanza tra due attributi discreti che corrisponde a 1
+	 * se sono diversi, a 0 se sono uguali
+	 */
+	double distance(Object a) {
+		//verifica se il valore dell'attributo sia uguale a quello di input
+		if(getValue().equals(a))
+			return 0;
+		else
+			return 1;
+	}
+}
---
+++ src/Tuple.java
@@ -0,0 +1,62 @@
+/**
+ * <p>Description: rappresenta una tupla come una coppia attributo-valore
+ * @author sante
+ *
+ */
+class Tuple {
+	private Item[] tuple;
+	
+	/**
+	 * Costruisce l'oggetto riferito da tuple
+	 * @param size numero di item che costituirà a tupla
+	 */
+	Tuple(int size){
+		tuple=new Item[size];
+	}
+	
+	int getLength() {
+		return tuple.length;
+	}
+	
+	Item get(int i) {
+		return tuple[i];
+	}
+	
+	void add(Item c,int i) {
+		tuple[i]=c;
+	}
+	
+	/**
+	 * Determina la distanza tra la tupla riferita da obj e la tupla corrente(riferita da this).
+	 * La distanza è ottenuta come la somma delle distanze tra gli item in posizioni eguali nelle
+	 * due tuple.
+	 * @param obj
+	 * @return
+	 */
+	double getDistance(Tuple obj) {
+		double dist=0;
+		for(int i=0;i<getLength();i++) {
+			dist=dist+this.get(i).distance(obj.get(i));
+		}
+		return dist;
+	}
+	
+	/**
+	 * Restituisce la media delle distanze tra la tupla corrente e
+	 * quelle ottenibili dalla righe della matrice in data 
+	 * aventi l'indice clusteredData
+	 * @param data 
+	 * @param clusteredData indice di riga
+	 * @return
+	 */
+	double avgDistance(Data data, int clusteredData[]){ 
+		double p=0.0,sumD=0.0; 
+		for(int i=0;i<clusteredData.length;i++){ 
+			//data.getItemSet(clusteredData[i]) mi restituisce la tupla di indice memorizzato in clusteredData[i]
+			double d= getDistance(data.getItemSet(clusteredData[i])); 
+			sumD+=d; 
+		}
+		p=sumD/clusteredData.length; 
+		return p; 
+	}
+}
